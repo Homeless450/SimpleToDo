8bb45f32d7540ae112dd155133388ddd9dd7acd0
Homeless450 <apocliptik450@gmail.com>
2023-08-28 16:00:27 +0300

    Added Iservice and Service repository

---
+++ DataLayer/Services/Service.cs
@@ -0,0 +1,51 @@
+﻿using DataLayer.Data;
+using DataLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DataLayer.Services
+{
+    internal class Service<T> : IService<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Service(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>(); //db.Categories
+            _db.Goals.Include(u => u.UserId);
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter)
+        {
+            IQueryable<T> query = dbSet;
+
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+
+    }
+}
