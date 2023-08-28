432994882c58989090153f658dd9c76e804e1468
Homeless450 <apocliptik450@gmail.com>
2023-08-28 14:12:21 +0300

    scaffolded identity

--- DataLayer/Data/ApplicationDbContext.cs
+++ DataLayer/Data/ApplicationDbContext.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Models;
 
 namespace DataLayer.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) : base( options )
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
+        }
+
+        public DbSet<Goal> Goals { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(
+                new User { FirstName = "FirstName1", LastName = "LastName1", Phone = "123321", Patronymic = "Namowich" },
+                new User { FirstName = "FirstName1", LastName = "LastName2", Phone = "123321", Patronymic = "Namowich" }
+                );
+
+            modelBuilder.Entity<Goal>().HasData(
+                new Goal { Id = 1, Name = "eat burger in the morning", EndDay = DateTime.Now.AddDays(2), StartDay = DateTime.Now, UserId = 1 },
+                new Goal { Id = 2, Name = "drink cola in the morning", EndDay = DateTime.Now.AddDays(2), StartDay = DateTime.Now, UserId = 1 },
+                new Goal { Id = 3, Name = "go to gym", EndDay = DateTime.Now.AddDays(3), StartDay = DateTime.Now, UserId = 2 },
+                new Goal { Id = 4, Name = "go to bass", EndDay = DateTime.Now.AddDays(4), StartDay = DateTime.Now, UserId = 2 }
+                );
+
         }
     }
 }
--- Models/Goal.cs
+++ Models/Goal.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class Goal
+    public class Goal
     {
         public int Id { get; set; }
         [Required]
         [MaxLength(100)]
         public string Name { get; set; }
         public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
         public DateTime StartDay { get; set; }
         public DateTime? EndDay { get; set; }
         public bool Repeating { get; set; }
