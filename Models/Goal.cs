e6d2ec58ff3b60712d193ecf0d569c0343a5031d
Homeless450 <apocliptik450@gmail.com>
2023-08-28 13:26:13 +0300

    Added Coals Model

---
+++ Models/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Goal
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDay { get; set; }
+        public DateTime? EndDay { get; set; }
+        public bool Repeating { get; set; }
+        public bool Completed { get; set; } = false;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Models
+{ 
+    public class User : IdentityUser<int>
+    {
+        public User()
+        {
+            Goals = new List<Goal>();
+        }
+
+        public virtual ICollection<Goal> Goals { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool ActivityStatus { get; set; }
+    }
+}
