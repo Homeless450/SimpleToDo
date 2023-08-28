aed70207af5d53039586e773d36cc96b11723a80
Homeless450 <apocliptik450@gmail.com>
2023-08-28 16:00:38 +0300

    Goal and User services

---
+++ DataLayer/Services/GoalService.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Data;
+using DataLayer.Interfaces;
+using Models;
+
+namespace DataLayer.Services
+{
+    internal class GoalService : Service<Goal>, IGoalService
+    {
+        private ApplicationDbContext _db;
+        public GoalService(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Goal goal)
+        {
+            _db.Update(goal);
+        }
+    }
+}
---
+++ DataLayer/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Data;
+using DataLayer.Interfaces;
+using Models;
+
+namespace DataLayer.Services
+{
+    internal class UserService : Service<User>, IUserService
+    {
+        private ApplicationDbContext _db;
+        public UserService(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(User user)
+        {
+            _db.Update(user);
+        }
+    }
+}
